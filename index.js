374ef5c07ff098a30e91fe549ffc995bbc0f8237
Raka <harshveerraka@gmail.com>
2024-04-15 10:35:03 +0530

    backend setup and made the css responsive
    user id and name is yet to be shown and also the image posting yet to be done

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const path = require("path");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Serve the static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Socket.IO connection handling
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Handle incoming messages
+  socket.on("chatMessage", (message) => {
+    // Broadcast the message to all connected clients
+    io.emit("chatMessage", message);
+  });
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
