4a65ccfb14a48e8b8683ae6e76c3bd5a5610cc0b
Raka <harshveerraka@gmail.com>
2024-04-30 23:10:20 +0530

    added usernames and the random user id to them
    previous messages are not visible to the new users

--- index.js
+++ index.js
@@ -10,19 +10,37 @@ const io = socketIo(server);
 // Serve the static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
 
+// Object to store user information (userId: username)
+const users = {};
+
+// Array to store chat messages
+const chatHistory = [];
+
 // Socket.IO connection handling
 io.on("connection", (socket) => {
   console.log("A user connected");
 
+  // Handle new user
+  socket.on("newUser", ({ userId, username }) => {
+    // Store user information
+    users[userId] = username;
+    // Send chat history to the new user
+    socket.emit("chatHistory", chatHistory);
+  });
+
   // Handle incoming messages
-  socket.on("chatMessage", (message) => {
+  socket.on("chatMessage", ({ userId, username, message }) => {
     // Broadcast the message to all connected clients
-    io.emit("chatMessage", message);
+    io.emit("chatMessage", {
+      userId,
+      username: users[userId], // Retrieve username using userId
+      message,
+    });
   });
 
   // Handle disconnection
   socket.on("disconnect", () => {
-    console.log("A user disconnected");
+    console.log("Someboady left the chat");
   });
 });
 
